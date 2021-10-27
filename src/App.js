a1240ea7a5adbc40bdac6f4f075178eb5de04c41
MarceloPorco <marceloporco621@gmail.com>
2021-10-26 21:09:50 -0300

    repo git

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+    <header>
+      <h5>Donnamiamora</h5>
+      <nav>
+        <a href="#">Tienda</a>
+        <a href="#">Nosotros</a>
+        <a href="#">Nuestras redes</a>
+      </nav>
+    </header>
+    </>
+  );
+}
+
+export default App;
